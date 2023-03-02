d81c134516a77f665dd9b27053807f99f9bcd89f
alikeli <eli-com2@yandex.ru>
2023-03-02 09:44:36 +0100

    done

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double d = toBeDivided/5;
+        System.out.println(d);
+          }
+
+    public static void main(String[] args) {
+        NumberDivider numberDivider = new NumberDivider();
+         numberDivider.divide(20);
+
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,19 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+
+        float sec = minutes * 60;
+        System.out.println(sec);
     }
+
+    public static void main(String[] args) {
+
+        TimeConvertor sec = new TimeConvertor();
+        sec.convert(10);
+
+
+    }
+
 }
+
+
